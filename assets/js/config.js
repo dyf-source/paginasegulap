da133f548b8e8ca84b913646f63313bcb1a5d807
dyf-source <unchalofelipe@gmail.com>
2026-05-27 13:28:42 -0300

    checkpoint erase db for now

--- assets/js/config.js
+++ assets/js/config.js
@@ -8,14 +8,3 @@ function getWhatsAppNumber() {
 function setWhatsAppNumber(num) {
   localStorage.setItem('segulap_whatsapp', num);
 }
-
-/* Sync WhatsApp config from the API server on page load (db.js must be loaded first) */
-if (typeof SegulapDB !== 'undefined') {
-  SegulapDB.ready(function() {
-    if (SegulapDB.isApiAvailable()) {
-      SegulapDB.getConfig('whatsapp').then(function(val) {
-        if (val) localStorage.setItem('segulap_whatsapp', val);
-      }).catch(function(){});
-    }
-  });
-}
